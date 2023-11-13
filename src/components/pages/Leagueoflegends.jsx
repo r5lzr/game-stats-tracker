aef75f0745df642d12b7dd485b6b236cc931c0b0
githubuser55555 <unreversable22@gmail.com>
2023-11-13 21:27:23 +0000

    Added search bar and background for LoL page

--- src/components/pages/Leagueoflegends.jsx
+++ src/components/pages/Leagueoflegends.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./Leagueoflegends.css"
 
 export const Leagueoflegends = () => {
   return (
-    <div>Leagueoflegends</div>
+    <div className='body-container'>
+      <div className='game-title'>
+        <h1>League of Legends</h1>
+      </div>
+      <div className='search-container'>
+        <input placeholder="Search..."/>
+      </div>
+    </div>
   )
 }
