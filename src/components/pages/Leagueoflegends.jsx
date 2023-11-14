4824e04abd286ca4f76c1561ffbaabd321e15ae1
githubuser55555 <unreversable22@gmail.com>
2023-11-14 03:18:38 +0000

    Updated search bar and added Select Region

--- src/components/pages/Leagueoflegends.jsx
+++ src/components/pages/Leagueoflegends.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Leagueoflegends.css"
 
 export const Leagueoflegends = () => {
+  const [isActive, setIsActive] = useState(false)
+  const [selected, setSelected] = useState("Select Region")
+  const options = ["EUW", "NA"]
+
   return (
     <div className='body-container'>
       <div className='game-title'>
         <h1>League of Legends</h1>
       </div>
+
       <div className='search-container'>
-        <input placeholder="Search..."/>
+        <input placeholder="Enter username..."/>
+        <div className='dropdown'>
+          <div className='dropdown-btn' onClick={() => setIsActive(!isActive)}>
+            {selected}
+            <span className='dropdown-icon'>&#9660;</span>
+          </div>
+          {isActive && (
+          <div className='dropdown-content'>
+            {options.map((option) => (
+            <div onClick={() => {setSelected(option); setIsActive(false);}} className='dropdown-item'>
+              {option}
+            </div>
+            ))}
+          </div>
+          )}
+        </div>
       </div>
     </div>
   )
