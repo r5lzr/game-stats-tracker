99fcaf10f570f9a6cc99d06374e7b4272f815eb1
githubuser55555 <unreversable22@gmail.com>
2024-07-31 18:02:07 +0100

    Profile search bar is functioning and able to query URL for players

--- app/ui/league-of-legends/search/search-bar.tsx
+++ app/ui/league-of-legends/search/search-bar.tsx
@@ -1,22 +1,47 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { IoSearch } from "react-icons/io5";
 import styles from "../profile.module.css";
 
 export function SearchBar() {
   const [isActive, setIsActive] = useState(false);
-  const [selected, setSelected] = useState("Region");
+  const [selected, setSelected] = useState("EUW");
   const options = ["EUW", "NA"];
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+  let region: string;
+
+  const onSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (selected === "EUW") {
+      region = "europe";
+    } else if (selected === "NA") {
+      region = "americas";
+    }
+
+    const search = searchQuery.replace("#", "/");
+    const encodedSearchQuery = encodeURI(search);
+    router.push(`/league-of-legends/${region}/${encodedSearchQuery}`);
+  };
 
   return (
-    <div className={styles["search-container"]}>
+    <div className={styles["input-container"]}>
       <div className={styles["search-icon"]}>
         <IoSearch size={30} />
       </div>
-      <input
-        className={styles["input-field"]}
-        placeholder="Search Riot ID, PLAYER#EUW1"
-      />
+      <form className={styles["form-field"]} onSubmit={onSearch}>
+        <input
+          value={searchQuery}
+          spellCheck="false"
+          className={styles["input-field"]}
+          placeholder="Player Name + #Tagline"
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+        />
+      </form>
       <div className={styles["dropdown"]}>
         <div
           className={styles["dropdown-btn"]}
