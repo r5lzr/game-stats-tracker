e99941ad1bebd0f8388d91211ef82090d91416bb
githubuser55555 <unreversable22@gmail.com>
2024-07-31 15:00:59 +0100

    Adjusted home search bar styling and changed placeholder value. Selecting region from dropdown now works with URL querying.

--- app/ui/home-search.tsx
+++ app/ui/home-search.tsx
@@ -6,27 +6,34 @@ import { useRouter } from "next/navigation";
 
 export function HomeSearch() {
   const [isActive, setIsActive] = useState(false);
-  const [selected, setSelected] = useState("Select Region");
+  const [selected, setSelected] = useState("EUW");
   const options = ["EUW", "NA"];
   const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
+  let region: string;
 
   const onSearch = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const encodedSearchQuery = encodeURI(searchQuery);
-    router.push(`/league-of-legends/${encodedSearchQuery}`);
+    if (selected === "EUW") {
+      region = "europe";
+    } else if (selected === "NA") {
+      region = "americas";
+    }
 
-    console.log("current query", encodedSearchQuery);
+    const search = searchQuery.replace("#", "/");
+    const encodedSearchQuery = encodeURI(search);
+    router.push(`/league-of-legends/${region}/${encodedSearchQuery}`);
   };
 
   return (
     <div className={styles["search-container"]}>
-      <form onSubmit={onSearch}>
+      <form className={styles["form-field"]} onSubmit={onSearch}>
         <input
           value={searchQuery}
+          spellCheck="false"
           className={styles["input-field"]}
-          placeholder="Enter username..."
+          placeholder="Player Name + #Tagline"
           onChange={(e) => {
             setSearchQuery(e.target.value);
           }}
