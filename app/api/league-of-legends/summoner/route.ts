d1434c767811432546354c213b621acf6475b48e
githubuser55555 <unreversable22@gmail.com>
2024-08-03 16:23:38 +0100

    Added NextRequest for summoner route.ts

--- app/api/league-of-legends/summoner/route.ts
+++ app/api/league-of-legends/summoner/route.ts
@@ -1,10 +1,11 @@
 import { RiotAPI, PlatformId } from "@fightmegg/riot-api";
+import { NextRequest } from "next/server";
 
 const RApi = new RiotAPI(process.env.RIOT_API_KEY as string, {
   cache: { cacheType: "local" },
 });
 
-export async function GET(request: { nextUrl: { searchParams: any } }) {
+export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
 
   const region = searchParams.get("region");
