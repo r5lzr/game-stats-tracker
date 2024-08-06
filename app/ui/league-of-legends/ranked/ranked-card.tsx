6b68f603fb7b615c9778e1cb283c16facad510a1
githubuser55555 <unreversable22@gmail.com>
2024-08-06 05:01:51 +0100

    Refactored ranked-card to make less API calls than required and prevent bottleneck error.

--- app/ui/league-of-legends/ranked/ranked-card.tsx
+++ app/ui/league-of-legends/ranked/ranked-card.tsx
@@ -14,17 +14,22 @@ async function getRankedInfo(summonerId: string, region: string) {
   return await res.json();
 }
 
-async function getPlayerRankSolo(summonerId: string, region: string) {
+async function getRankedActivity(summonerId: string, region: string) {
   let rankedSoloActivity: any[] | boolean = [];
+  let rankedFlexActivity: any[] | boolean = [];
 
   const rankedData: RankedInfo[] = await getRankedInfo(summonerId, region);
 
-  const checkActivity = rankedData.find(
+  const checkSolo = rankedData.find(
     (rankInfo) => rankInfo.queueType === "RANKED_SOLO_5x5"
   );
 
-  if (checkActivity) {
-    const { tier, rank, leaguePoints, wins, losses } = checkActivity;
+  const checkFlex = rankedData.find(
+    (rankInfo) => rankInfo.queueType === "RANKED_FLEX_SR"
+  );
+
+  if (checkSolo) {
+    const { tier, rank, leaguePoints, wins, losses } = checkSolo;
     rankedSoloActivity[0] = tier;
     rankedSoloActivity[1] = rank;
     rankedSoloActivity[2] = `${leaguePoints} LP`;
@@ -34,20 +39,8 @@ async function getPlayerRankSolo(summonerId: string, region: string) {
     rankedSoloActivity = false;
   }
 
-  return rankedSoloActivity;
-}
-
-async function getPlayerRankFlex(summonerId: string, region: string) {
-  let rankedFlexActivity: any[] | boolean = [];
-
-  const rankedData: RankedInfo[] = await getRankedInfo(summonerId, region);
-
-  const checkActivity = rankedData.find(
-    (rankInfo) => rankInfo.queueType === "RANKED_FLEX_SR"
-  );
-
-  if (checkActivity) {
-    const { tier, rank, leaguePoints, wins, losses } = checkActivity;
+  if (checkFlex) {
+    const { tier, rank, leaguePoints, wins, losses } = checkFlex;
     rankedFlexActivity[0] = tier;
     rankedFlexActivity[1] = rank;
     rankedFlexActivity[2] = `${leaguePoints} LP`;
@@ -57,7 +50,7 @@ async function getPlayerRankFlex(summonerId: string, region: string) {
     rankedFlexActivity = false;
   }
 
-  return rankedFlexActivity;
+  return [rankedSoloActivity, rankedFlexActivity];
 }
 
 export async function Ranked({
@@ -67,19 +60,16 @@ export async function Ranked({
   summonerId: string;
   region: string;
 }) {
-  const [rankedSoloInfo, rankedFlexInfo] = await Promise.all([
-    getPlayerRankSolo(summonerId, region),
-    getPlayerRankFlex(summonerId, region),
-  ]);
+  const rankedActivity = await getRankedActivity(summonerId, region);
 
   return (
     <>
       <div className={styles["ranked-title"]}>Current rank</div>
       <div className={styles["ranked-solo"]}>
-        <RankedQueue label="Ranked Solo" rankedInfo={rankedSoloInfo} />
+        <RankedQueue label="Ranked Solo" rankedInfo={rankedActivity[0]} />
       </div>
       <div className={styles["ranked-flex"]}>
-        <RankedQueue label="Ranked Flex" rankedInfo={rankedFlexInfo} />
+        <RankedQueue label="Ranked Flex" rankedInfo={rankedActivity[1]} />
       </div>
     </>
   );
