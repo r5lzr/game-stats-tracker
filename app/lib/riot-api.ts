1a42fb3825b3871318e990d394b49c7ae2ef9c02
GameMakerDeveloper <108896369+GameMakerDeveloper@users.noreply.github.com>
2024-08-14 15:18:27 +0100

    Should fix problems

--- app/lib/riot-api.ts
+++ app/lib/riot-api.ts
@@ -1,5 +1,4 @@
-// @ts-nocheck
-import { RiotAPI, RiotAPITypes } from "@fightmegg/riot-api";
+import { RiotAPI, RiotAPITypes, PlatformId } from "@fightmegg/riot-api";
 import { regionToCluster } from "@fightmegg/riot-api/dist/esm/utils";
 
 const RApi = new RiotAPI(process.env.RIOT_API_KEY as string, {
@@ -9,43 +8,43 @@ const RApi = new RiotAPI(process.env.RIOT_API_KEY as string, {
 export async function getMatches(
   username: string,
   tagLine: string,
-  region: string
+  region: RiotAPITypes.LoLRegion
 ) {
   const platformId = regionToCluster(region);
 
   const riotAccount = await RApi.account.getByRiotId({
+    // @ts-ignore
     region: platformId,
     gameName: username,
     tagLine,
   });
 
   const matches = [];
-  for (const matchId of await RApi.matchV5.getIdsByPuuid({
-    cluster: platformId,
-    puuid: riotAccount.puuid,
-  })) {
-    matches.push(
-      await RApi.matchV5.getMatchById({ cluster: platformId, matchId })
-    );
+
+  if (platformId !== PlatformId.ESPORTS) {
+    for (const matchId of await RApi.matchV5.getIdsByPuuid({
+      cluster: platformId,
+      puuid: riotAccount.puuid,
+    })) {
+      matches.push(
+        await RApi.matchV5.getMatchById({ cluster: platformId, matchId })
+      );
+    }
   }
 
   return matches;
 }
 
-export async function getSummoner(summonerId: string, region: string) {
-  const platformId = regionToCluster(region);
-
+export async function getSummoner(summonerId: string, region: RiotAPITypes.LoLRegion) {
   return await RApi.summoner.getBySummonerId({
-    region: platformId,
+    region: region,
     summonerId: summonerId,
   });
 }
 
-export async function getRankedInfo(summonerId: string, region: string) {
-  const platformId = regionToCluster(region);
-
+export async function getRankedInfo(summonerId: string, region: RiotAPITypes.LoLRegion) {
   return await RApi.league.getEntriesBySummonerId({
-    region: platformId,
+    region: region,
     summonerId: summonerId,
   });
 }
